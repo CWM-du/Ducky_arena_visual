8cfc670d2907f0d44f262eaa9ffd97c8a5eb6e7c
H.Silva <trindadeproduction@gmail.com>
2026-09-14 12:34:10 +0200

    Delete duckies directory

--- duckies/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class DuckiesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "duckies"
-    verbose_name = "Ducky"
--- duckies/static/duckies/js/app.js
+++
@@ -1,2 +0,0 @@
-// Reserved for progressive enhancements. Core actions intentionally use
-// standard Django forms so functionality remains available without JavaScript.
